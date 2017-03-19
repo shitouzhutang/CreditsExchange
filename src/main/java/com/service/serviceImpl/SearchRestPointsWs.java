c79f73d0f6e1c8ea8b09144b362c73de6129de4f
shitouzhutang <mawenhaoh@foxmail.com>
2017-03-19 16:08:09 +0800

    添加注释

--- src/main/java/com/service/serviceImpl/SearchRestPointsWs.java
+++ src/main/java/com/service/serviceImpl/SearchRestPointsWs.java
@@ -1,7 +1,6 @@
 package com.service.serviceImpl;
 
 import com.model.po.CsbHttpResp;
-import com.model.po.GetPointLimitCrmidResp;
 import com.model.po.SearchRestPointsReq;
 import com.model.po.SearchRestPointsResp;
 import com.utils.CsbUtil;
@@ -18,7 +17,7 @@ import java.util.Date;
 public class SearchRestPointsWs {
     private static  final Logger log= LoggerFactory.getLogger(SearchRestPointsWs.class);
 
-    private static final String WS_REQ_XML = "soap/SearchRestPoints.xml";
+    private static final String WS_REQ_XML = "soap/SearchRestPointsReq.xml";
 
     private static String reqXml = "";
 
@@ -26,6 +25,12 @@ public class SearchRestPointsWs {
         reqXml = CsbUtil.loadWsXml(WS_REQ_XML);  //读取classpath下的xml文件内容，放在内存中
     }
 
+    /**
+     * 根据用户编号和设备编号查询用户积分余额
+     * @param customNo
+     * @param serialNo
+     * @return
+     */
     public static String getRestPoints(String customNo,String serialNo){
         String wsAddress="http://10.145.205.53:7805/openit/class_1?ServiceName=SearchRestPoints&ServiceVer=1.0&Consumer=ZZSLPT";
         SearchRestPointsResp resp=new SearchRestPointsResp();
@@ -37,11 +42,22 @@ public class SearchRestPointsWs {
         req.setStartDate(date);
         req.setSource("ZZSLPT");
         resp= SearchRestPointsWs.searchRestPoints(req,wsAddress);
+        resp.getPoints();
 
 
         return "积分余额";
     }
 
+    /**
+     * 调用webservice查询用户积分余额
+     *
+     * 测试地址：http://test.csb.sh.ctc.com:7805/openit/class_1?ServiceName=SearchRestPoints&ServiceVer=1.0&Consumer=
+     仿真地址：http://10.7.18.12:7805/openit/class_1?ServiceName=SearchRestPoints&ServiceVer=1.0&Consumer=
+     生产地址：http://csb.sh.ctc.com:7805/openit/class_1?ServiceName=SearchRestPoints&ServiceVer=1.0&Consumer=
+     * @param req
+     * @param wsAddress
+     * @return
+     */
     public static SearchRestPointsResp searchRestPoints(SearchRestPointsReq req,String wsAddress){
         SearchRestPointsResp resp=null;
         try {
